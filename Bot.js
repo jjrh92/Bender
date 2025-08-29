083979cfe143edfdaac531be3628f3b1b7811096
Julio Reyes <juliojesusreyes@gmail.com>
2025-08-29 22:13:07 +0200

    Just changing an emoji

--- Bot.js
+++ Bot.js
@@ -79,12 +79,12 @@ discordClient.on("voiceStateUpdate", (oldState, newState) => {
   if (!oldState.channelId && newState.channelId) {
     // User joined a voice channel
     sendTelegramMessage(
-      `<code>🗣️📢\n\nHeads up: The user ${user.username} has connected to ${newState.channel.name}.\n\n🟢🟢🟢</code>`
+      `<code>🗣️🗣️🗣️\n\nHeads up: The user ${user.username} has connected to ${newState.channel.name}.\n\n🟢🟢🟢</code>`
     );
   } else if (oldState.channelId && !newState.channelId) {
     // User left a voice channel
     sendTelegramMessage(
-      `<code>🗣️📢\n\nHeads Up: The user ${user.username} has disconnected from ${oldState.channel.name}.\n\n🔴🔴🔴</code>`
+      `<code>🗣️🗣️🗣️\n\nHeads Up: The user ${user.username} has disconnected from ${oldState.channel.name}.\n\n🔴🔴🔴</code>`
     );
   }
 });
